7037cf2c95245c6b23e24518c48f9e6c5e24a873
andrade._.g <gabriel.vinicius.andrade@escola.pr.gov.br>
2026-03-09 08:54:36 -0300

    Add files via upload

---
+++ aula1.js
@@ -0,0 +1,8 @@
+let nome = "Gabriel"
+let altura = 1.80
+let idade = 15
+let pele = "branca"
+let cabelo = "castanho claro"
+let uniforme = "uniforme do kennedy"
+let curso = "desenvolvimento de sistemas"
+console.log("Olá, meu nome é " + nome + "! Possuo " + idade + " anos! " + "minha altura é " + altura + ", minha cor de pele é " + pele + ", cabelos " + cabelo + ", estou usando o " + uniforme + " e estou fazendo o curso de " + curso)
