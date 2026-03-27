22cb066a28087a19869cb64dbf9de9d29f9d9dea
andrade._.g <gabriel.vinicius.andrade@escola.pr.gov.br>
2026-03-26 22:25:05 -0300

    Add files via upload

--- aula7projeto1.js
+++ aula7projeto1.js
@@ -12,7 +12,43 @@ Imagine que você abriu uma loja:
     - remove um elemento pelo índice
     - mostra estoque
 */
+
 const produtos = ['Chuteira Society', 'Bola de Futebol de Campo',' Caneleiras', 'Luvas de Goleiro', 'Bomba de Ar', 'Bola de Vôlei Indoor', 'Joelheiras', 'Rede de Vôlei', 'Manguitos', 'Tênis de Quadra']
 const valores = ['R$ 220,00', 'R$ 130,00', 'R$ 45,00', 'R$ 110,00', 'R$ 35,00', 'R$ 160,00', 'R$ 85,00', 'R$ 120,00',' R$ 50,00', 'R$ 350,00']
 const qualidade = ['Intermediária', 'Profissional', 'Básica', 'Intermediária',' Padrão', 'Premium', 'Alta', 'Residencial', 'Performance', 'Profissional']
 const estoque = ['15 unidades', '24 unidades', '40 unidades', '12 unidades', '50 unidades',' 18 unidades',' 35 unidades', '08 unidades',' 22 unidades', '10 unidades']
+
+function mostrarESTOQUE(){
+     let contador = 0 
+    while(contador <produtos.length){
+         console.log(produtos[contador] + " - " + valores[contador] + " - " + estoque[contador]) 
+         contador++
+         }
+        
+    }
+     function adicionarPRODUTO() { 
+        produtos.push("Macarrão")
+        valores.push("R$ 17,00")
+        estoque.push("23 unidades") 
+    } 
+    
+    function remorerPRODUTO(){
+         produtos.splice(10, 1)
+          valores.splice(10, 1) 
+          estoque.splice(10, 1)
+         }
+         
+         mostrarESTOQUE() 
+
+         adicionarPRODUTO()
+
+          console.log("-----------------------------------") 
+
+          mostrarESTOQUE()
+
+           remorerPRODUTO()
+
+            console.log("-----------------------------------") 
+            
+            mostrarESTOQUE()
+
