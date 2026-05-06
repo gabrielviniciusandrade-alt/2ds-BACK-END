fffe5c32a9a691a387e6da599c87ee5b2379e122
andrade._.g <gabriel.vinicius.andrade@escola.pr.gov.br>
2026-05-06 09:33:28 -0300

    Add files via upload

---
+++ recuperacao.js
@@ -0,0 +1,5 @@
+let nome = "Gabriel"
+let altura = 1.80
+let serie  = "2DS"
+let turma = "B"
+console.log("Olá, meu nome é " + nome + "! minha altura é " + altura + " ! " + "minha serie é " + serie + ", minha turma é " + turma )
