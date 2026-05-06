7907255ef6986213797285db18ef261e0674b56d
andrade._.g <gabriel.vinicius.andrade@escola.pr.gov.br>
2026-05-06 09:41:03 -0300

    Add files via upload

--- recuperacao.js
+++ recuperacao.js
@@ -1,5 +1,15 @@
-let nome = "Gabriel"
-let altura = 1.80
-let serie  = "2DS"
-let turma = "B"
-console.log("Olá, meu nome é " + nome + "! minha altura é " + altura + " ! " + "minha serie é " + serie + ", minha turma é " + turma )
+const modelo = {
+    ESPORTE : "Futebol",
+    ESPORTE : "futsal",
+    ESPORTE : "basquete" ,
+    
+}
+
+const esportes = [
+    {
+        // Esporte 1
+    },
+    {
+        // Esporte 2 
+    }
+]
